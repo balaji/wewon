ab7ae0fdf96199aff04c9e897640cd095c336695
= <=>
2011-05-17 16:16:55 +0530

    added table view

--- home.rb
+++ home.rb
@@ -26,7 +26,7 @@ post '/log-in' do
   if c.response_code == 401
     redirect '/unauthorized'
   else
-    name = Nokogiri::XML(c.body_str).at_xpath('Assets/Asset/Attribute[@name="Name"]').to_s
+    name = Nokogiri::XML(c.body_str).at_xpath('Assets/Asset/Attribute[@name="Name"]/text()').to_s
     session[:user_name] = name
     session[:login_name] = params[:name]
     session[:password] = params[:password]
@@ -39,5 +39,15 @@ get '/unauthorized' do
 end
 
 get '/success' do
+  c = Curl::Easy.new("https://#{session[:login_name]}:#{session[:password]}@www10.v1host.com/sandp/rest-1.v1/Data/Story\?sel\=Name,Estimate,Number,Status.Name\&where\=%28Team.Name\=%27SAS%20EMEA%20Integration%20Team%27\;Timebox.Name\=%27Sprint%207%27%29")
+  c.perform
+  @data = []
+  Nokogiri::XML(c.body_str).xpath('//Assets/Asset').each do |row|
+    @data.push({:number => row.at_xpath('Attribute[@name="Number"]/text()').to_s,
+    :status => row.at_xpath('Attribute[@name="Status.Name"]/text()').to_s,
+    :name => row.at_xpath('Attribute[@name="Name"]/text()').to_s,
+    :estimate => row.at_xpath('Attribute[@name="Estimate"]/text()').to_s})
+  end
+  puts @data
   haml :welcome
 end
